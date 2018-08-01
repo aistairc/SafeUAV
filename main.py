91f40e66b9d2e5979969f57d28c620af5fb8a9cc
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2018-08-01 14:34:00 +0300

    Added retrain and callbacks. Moved getNumDims in library

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import sys
+import os
 import numpy as np
 import torch.optim as optim
 from argparse import ArgumentParser
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from neural_wrappers.readers import CitySimReader
 from neural_wrappers.models import ModelUNetDilatedConv
 from neural_wrappers.pytorch import maybeCuda
+from neural_wrappers.callbacks import SaveHistory, SaveModels, Callback, PlotMetricsCallback
 
 from unet_tiny_sum import ModelUNetTinySum
 from test_dataset import testDataset
 from loss import l2_loss, classification_loss
-
 
 def getArgs():
 	parser = ArgumentParser()
@@ -18,6 +20,7 @@ def getArgs():
 	parser.add_argument("dataset_path", help="Path to dataset")
 
 	# Dataset and Data stuff
+	parser.add_argument("--dir")
 	parser.add_argument("--data_dims", default="rgb")
 	parser.add_argument("--label_dims", default="depth")
 
@@ -27,39 +30,37 @@ def getArgs():
 	parser.add_argument("--optimizer", type=str)
 	parser.add_argument("--learning_rate", type=float)
 	parser.add_argument("--momentum", default=0.9, type=float)
+	parser.add_argument("--patience", default=4, type=int)
 
 	# Model stuff
 	parser.add_argument("--model", type=str)
+	parser.add_argument("--weights_file")
 
 	args = parser.parse_args()
-	assert args.type in ("test_dataset", "train")
+	assert args.type in ("test_dataset", "train", "retrain")
 	assert args.task in ("classification", "regression")
 	if not args.type in ("test_dataset", ):
 		assert args.model in ("unet_big_concatenate", "unet_tiny_sum")
-	# assert args.type in ("train", "train_pretrained", "test", "retrain", "export_new_labels", "test_videos")
+	if args.type == "retrain":
+		args.weights_file = os.path.abspath(args.weights_file)
 	args.data_dims = args.data_dims.split(",")
 	args.label_dims = args.label_dims.split(",")
 
 	return args
 
-def getModel(args, reader):
-	def getNumDims(allDims, Dims, hvnNumDims):
-		numDims = 0
-		for dim in Dims:
-			assert dim in allDims
-			if dim == "rgb":
-				numDims += 3
-			elif dim == "depth":
-				numDims += 1
-			elif dim == "hvn_gt_p1":
-				numDims += hvnNumDims
-			else:
-				assert False, "%s" % dim
-		return numDims
+class SchedulerCallback(Callback):
+	def __init__(self, optimizer, patience):
+		self.scheduler = ReduceLROnPlateau(optimizer, "min", factor=0.1, patience=patience, eps=1e-8)
 
-	hvnNumDims = 2 if args.task == "regression" else 1
-	allDims = reader.allDims
-	dIn, dOut = getNumDims(allDims, args.data_dims, hvnNumDims), getNumDims(allDims, args.label_dims, hvnNumDims)
+	def onEpochEnd(self, **kwargs):
+		if not kwargs["validationMetrics"]:
+			loss = kwargs["trainMetrics"]["Loss"]
+		else:
+			loss = kwargs["validationMetrics"]["Loss"]
+		self.scheduler.step(loss)
+
+def getModel(args, reader):
+	dIn, dOut = reader.getNumDimensions(args.data_dims), reader.getNumDimensions(args.label_dims)
 	if args.model == "unet_big_concatenate":
 		model = ModelUNetDilatedConv(dIn=dIn, dOut=dOut, numFilters=64, bottleneckMode="dilate2_serial_concatenate")
 	elif args.model == "unet_tiny_sum":
@@ -79,6 +80,13 @@ def setOptimizer(args, model):
 	else:
 		assert False, "%s" % args.optimizer
 
+def changeDirectory(Dir, expectExist):
+	assert os.path.exists(Dir) == expectExist
+	print("Changing to working directory:", Dir)
+	if expectExist == False:
+		os.makedirs(Dir)
+	os.chdir(Dir)
+
 def main():
 	args = getArgs()
 
@@ -97,14 +105,29 @@ def main():
 	model.setCriterion(criterion)
 	print(model.summary())
 
-	if args.type == "train":
-		generator = reader.iterate("train", miniBatchSize=args.batch_size)
-		steps = reader.getNumIterations("train", miniBatchSize=args.batch_size)
-		valGenerator = reader.iterate("train", miniBatchSize=args.batch_size)
-		valSteps = reader.getNumIterations("train", miniBatchSize=args.batch_size)
+	generator = reader.iterate("train", miniBatchSize=args.batch_size)
+	steps = reader.getNumIterations("train", miniBatchSize=args.batch_size)
+	valGenerator = reader.iterate("validation", miniBatchSize=args.batch_size)
+	valSteps = reader.getNumIterations("validation", miniBatchSize=args.batch_size)
 
-		model.train_generator(generator, steps, numEpochs=args.num_epochs, callbacks=[], \
+	if args.type == "train":
+		changeDirectory(args.dir, expectExist=False)
+
+		callbacks = [SaveModels(type="all"), SaveHistory("history.txt", mode="write"), \
+			PlotMetricsCallback(["Loss"], ["min"]), SchedulerCallback(model.optimizer, args.patience)]
+		callbacks[1].file.write(reader.summary())
+		callbacks[1].file.write(model.summary())
+
+		model.train()
+		model.train_generator(generator, stepsPerEpoch=steps, numEpochs=args.num_epochs, callbacks=callbacks, \
 			validationGenerator=valGenerator, validationSteps=valSteps)
+	elif args.type == "retrain":
+		changeDirectory(args.dir, expectExist=True)
+		model.loadModel(args.weights_file)
+
+		model.train()
+		model.train_generator(generator, stepsPerEpoch=steps, numEpochs=args.num_epochs, \
+			callbacks=None, validationGenerator=valGenerator, validationSteps=valSteps)
 	else:
 		pass
 
